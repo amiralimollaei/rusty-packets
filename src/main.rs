bbacfa57927bc8e6a3e62af69b595bedbbf0b4b9
AmirAli Mollaei <me.amiralimollaei@gmail.com>
2026-01-12 23:57:34 +0330

    cleanups

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use minecraft::application::Client;
 
 use crate::utils::logging::{get_logger, set_log_level};
 
-
 fn main() {
     // set debug
     if cfg!(debug_assertions) {
@@ -14,23 +13,28 @@ fn main() {
 
     // initialize the client
     let mut client = Client::new("localhost", 55916, "Entity");
-    
+
     // ping the server
     let statusresponse = client.status_request();
     get_logger().debug(format!("StatusResponse: {:?}", statusresponse));
-    get_logger().info(format!("Status Response:\n{}{}", " ".repeat(53), statusresponse.get_players().get_players_count()));
+    get_logger().info(format!(
+        "Status Response:\n{}{}",
+        " ".repeat(53),
+        statusresponse.get_players().get_players_count()
+    ));
     get_logger().info(format!("{}", statusresponse.get_description_text()));
-    get_logger().info(format!("players:\n{}", statusresponse.get_players().get_players_list().join("\n")));
-    match statusresponse.get_favicon() {
-        None => {}
-        Some(c) => {
-            std::fs::write(
-                "favicon.png",
-                c.get_raw_image().as_ref().unwrap(),
-            ).expect("IO Error");
-        }
+    get_logger().info(format!(
+        "players:\n{}",
+        statusresponse.get_players().get_players_list().join("\n")
+    ));
+    if let Some(image) = statusresponse.get_favicon() {
+        std::fs::write(
+            "favicon.png",
+            image.get_raw_image().expect("unable to load server icon"),
+        )
+        .expect("IO Error");
     }
-    
+
     // join the server
     client.spawn_player();
 }
